9c79dec8089be438558d41881874fc34f767e953
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-02-24 13:01:49 +0900

    sine wave

---
+++ public/src/app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+
+const AudioContext = window.AudioContext || window.webkitAudioContext;
+const actx = new AudioContext();
+const osc = actx.createOscillator();
+osc.connect(actx.destination);
+osc.start();
+console.log('おわり');
+
+
+// 着火のおまじない
+const eventName = typeof document.ontouchend !== 'undefined' ? 'touchend' : 'mouseup';
+document.addEventListener(eventName, initAudioContext);
+
+function initAudioContext(){
+  document.removeEventListener(eventName, initAudioContext);
+  // wake up AudioContext
+  actx.resume();
+}
+
